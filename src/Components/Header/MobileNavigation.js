45028ffb80aada07c34f753b24513fc8dbd1ea26
Boopathi-king <boopathiboopathi31@gmail.com>
2024-09-26 13:48:21 +0530

    footer media quries updated

--- src/Components/Header/MobileNavigation.js
+++ src/Components/Header/MobileNavigation.js
@@ -1,34 +1,29 @@
-import Navlinks from "./Navlinks";
-import './Navbar.css'
-import {MdOutlineMenu} from 'react-icons/md';
+
+import Navlinks from './Navlinks'
+import './Navbar.css';
+import {MdOutlineMenu} from 'react-icons/md'
 import { useState } from "react";
 import {MdClose} from 'react-icons/md';
 
 
-const MobileNavigation = () => {
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-
-    const closeMenu = () => setClick(false);
-
+const MobileNavigation = () =>{
+    const [click, setclick] = useState(false);
 
     const Hamburger = <MdOutlineMenu className="HamburgerMenu"
-    size="30px" color="black"
-    onClick={() => setClick(!click)}
-    />
+           size="30px" color="black"
+           onClick={() => setclick(!click)} />
 
     const Close = <MdClose className="HamburgerMenu"
             size="30px" color="black"
-           onClick={() => handleClick(!click)} />
+           onClick={() => setclick(!click)} />
 
-
-  return (
-    <nav className="MobileNavigation">
-        <h2 className="logo">CHICITY</h2>
-        {click ? Close : Hamburger}
-        {click && <Navlinks isClicked={true} closeMenu={closeMenu}/>}
-    </nav>
-  )
+    return(
+        <nav className="MobileNavigation">
+             <h2 className="logo">CHICITY</h2>
+             { click ? Close : Hamburger}
+             {click && <Navlinks />}
+        </nav>
+    )
 }
 
 export default MobileNavigation;
